95b925ced9d68a3bfbfb47037e338511a7899cb0
Firm-Soft <evan.w.mclaughlin@gmail.com>
2020-12-30 12:55:42 -0500

    Push out non-ivy fix

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,6 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { getAuthSubRoutes, PxbAuthGuard, AUTH_ROUTES } from '@pxblue/angular-auth-workflow';
+import {
+  PxbAuthGuard,
+  AUTH_ROUTES,
+  PxbLoginComponent,
+  PxbResetPasswordComponent,
+  PxbForgotPasswordComponent,
+  PxbCreateAccountComponent,
+  PxbContactSupportComponent,
+  PxbCreateAccountInviteComponent
+} from '@pxblue/angular-auth-workflow';
 import { HomeComponent } from './pages/home/home.component';
 import { AuthComponent } from './pages/auth/auth.component';
 
@@ -8,7 +17,18 @@ import { AuthComponent } from './pages/auth/auth.component';
 AUTH_ROUTES.AUTH_WORKFLOW = 'auth';
 AUTH_ROUTES.CONTACT_SUPPORT = 'assistance';
 
-const authWorkflowRoutes = getAuthSubRoutes();
+/*
+Non-ivy projects who desire route-name customization will have to declare their own routes.
+const authWorkflowRoutes = [
+  { path: '', redirectTo: AUTH_ROUTES.LOGIN, pathMatch: 'full' },
+  { path: AUTH_ROUTES.LOGIN, component: PxbLoginComponent },
+  { path: AUTH_ROUTES.RESET_PASSWORD, component: PxbResetPasswordComponent },
+  { path: AUTH_ROUTES.FORGOT_PASSWORD, component: PxbForgotPasswordComponent },
+  { path: AUTH_ROUTES.CREATE_ACCOUNT, component: PxbCreateAccountComponent },
+  { path: 'assistance', component: PxbContactSupportComponent },
+  { path: AUTH_ROUTES.CREATE_ACCOUNT_INVITE, component: PxbCreateAccountInviteComponent },
+];
+ */
 const routes: Routes = [
     { path: '', redirectTo: AUTH_ROUTES.AUTH_WORKFLOW, pathMatch: 'full' },
     { path: AUTH_ROUTES.AUTH_WORKFLOW, component: AuthComponent, children: authWorkflowRoutes },
